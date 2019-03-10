d8c707488dc2e1ca6241a2fcf537e56d91272e26
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-03-09 17:17:57 -0800

    - Better Drag

--- ThePinkPodcast/Views/PlayerDetailsView.swift
+++ ThePinkPodcast/Views/PlayerDetailsView.swift
@@ -118,13 +118,13 @@ class PlayerDetailsView: UIView {
     @objc func handleDismissalPan(gesture: UIPanGestureRecognizer) {
         if gesture.state == .changed {
             let translation = gesture.translation(in: self.superview)
-            maximizedStackView.transform = CGAffineTransform(translationX: 0, y: translation.y)
+            self.transform = CGAffineTransform(translationX: 0, y: translation.y)
         } else if gesture.state == .ended {
             let translation = gesture.translation(in: self.superview)
 
             UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
                 
-                self.maximizedStackView.transform = .identity
+                self.transform = .identity
 
                 if translation.y > 200 {
                     let mainTabBarController = UIApplication.mainTabBarController()
